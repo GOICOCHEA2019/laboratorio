d6ef266b22e2ace45e8c2e06603dff7faa15e002
Fidel Latorre <fidellatorreabad@gmail.com>
2020-01-17 20:15:01 -0500

    proveedores

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('productos', 'ProductosController');
+Route::resource('unidades', 'UnidadesController');
+Route::resource('categorias', 'CategoriasController');
+Route::resource('proveedores', 'ProveedoresController');
